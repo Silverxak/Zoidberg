d314bc6d1cffa18ad2f7d31c1b7fdff6ea8d7e3e
silvergt <silverxak@gmail.com>
2015-10-13 23:47:32 +0300

    st3

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-	$('#tab_newreleases_content .tab_item.app_impression_tracked .tab_item_content .tab_item_details .tab_item_top_tags').before('<span class="zoidberg-cards-icon"></span>');
+	$('#tab_newreleases_content .tab_item.app_impression_tracked .tab_item_content .tab_item_details .tab_item_top_tags').before('<span class="platform_img zoidberg-cards-icon"></span>');
 });
