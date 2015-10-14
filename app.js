581cea4360d901f760dfd64ee5ce2a229f2e0b10
Silverxak <silverxak@gmail.com>
2015-10-14 14:15:21 +0300

    regexp

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	$('#tab_newreleases_content .tab_item.app_impression_tracked .tab_item_content .tab_item_details .tab_item_top_tags').before('<span class="platform_img zoidberg-cards-icon"></span>');
+	$('.tab_item_top_tags:first').each(function(){
+		var parser = $(this).parents('.tab_item_content').children('.tab_item_name').text();
+		alert(parser);
+		var regExp = /.+(?=\s-)/;
+		var gname = regExp.exec(parser);
+		alert(gname);
+		$(this).before('<a target="_blank" href="http://steamcommunity.com/market/search?q=' + gname + '#p1_quantity_desc"><span class="platform_img zoidberg-cards-icon"></span></a>');
+	});
+
+/*	$('.zoidberg-cards-icon').each(function(){
+		$(this).on('click', function(){
+			$(this).click();
+		});
+	});
+
+	$('.tab_item_overlay').css('z-index','0');*/
 });
