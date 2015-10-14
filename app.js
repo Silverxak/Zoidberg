06113535951cd9a52930770539acfb059858f65f
Silverxak <silverxak@gmail.com>
2015-10-14 17:58:58 +0300

    link

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
-	$('.tab_item_top_tags:first').each(function(){
+	$('.tab_item_top_tags').each(function(){
 		var parser = $(this).parents('.tab_item_content').children('.tab_item_name').text();
-		alert(parser);
 		var regExp = /.+(?=\s-)/;
-		var gname = regExp.exec(parser);
-		alert(gname);
-		$(this).before('<a target="_blank" href="http://steamcommunity.com/market/search?q=' + gname + '#p1_quantity_desc"><span class="platform_img zoidberg-cards-icon"></span></a>');
+		var regExp2 = /.+/;
+		if(regExp.test(parser))
+			var gname = regExp.exec(parser);
+		else
+			var gname = regExp2.exec(parser);
+		$(this).before('<a target="_blank" class="link-to-card" href="http://steamcommunity.com/market/search?q=' + gname + '#p1_quantity_desc"><span class="platform_img zoidberg-cards-icon"></span></a>');
 	});
 
-/*	$('.zoidberg-cards-icon').each(function(){
-		$(this).on('click', function(){
-			$(this).click();
-		});
+	//$('.tab_item_overlay').css('z-index','0');
+	$('.tab_item_overlay').remove();
+
+	$('.link-to-card:first').ready(function(){
+		/*var link = $(this).attr('href');
+		console.log(link);*/
+		/*$('.link-to-card').on('click',function(){
+			//window.open(link); 
+			alert($(this).attr('href'));
+		});*/
+		var link = $(this).attr('href');
+		alert(link);
 	});
 
-	$('.tab_item_overlay').css('z-index','0');*/
+
 });
