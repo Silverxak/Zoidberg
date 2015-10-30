6370fa474d9e8087dfa6242e448eae1db5fd6240
Silverxak <silverxak@gmail.com>
2015-10-30 17:38:51 +0300

    sdb.png

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 //$(document).ready(function(){
 
-	$('.tab_item_top_tags').each(function(){
-		var parser = $(this).parents('.tab_item_content').children('.tab_item_name').text();
-		var regExp = /.+(?=\s-)/;
-		var regExp2 = /.+/;
-		var URL = encodeURIComponent((regExp.test(parser)) ? regExp.exec(parser) : regExp2.exec(parser));
-		$(this).before('<a target="_blank" class="link-to-card" href="http://steamcommunity.com/market/search?q=' + URL + '#p1_quantity_desc"><span class="platform_img zoidberg-cards-icon"></span></a>');
-	});
+$('.tab_item_top_tags').each(function() {
+	var parser = $(this).parents('.tab_item_content').children('.tab_item_name').text();
+	var regExp = /.+(?=\s-)/;
+	var regExp2 = /.+/;
+	var URL = encodeURIComponent((regExp.test(parser)) ? regExp.exec(parser) : regExp2.exec(parser));
+	$(this).before('<a target="_blank" class="link-to-card" href="http://steamcommunity.com/market/search?q=' + URL + '#p1_quantity_desc"><span class="platform_img zoidberg-cards-icon"></span></a>');
+});
 
-	$('.link-to-card').each(function(){
-		$(this).after('<a target="_blank" class="link-to-steamdb" href="https://steamdb.info/app/' + $(this).parents('.tab_item').attr('data-ds-appid') + '/graphs/"><span class="platform_img steamdb-icon"></span></a>');
-	});
+$('.link-to-card').each(function() {
+	$(this).after('<a target="_blank" class="link-to-steamdb" href="https://steamdb.info/app/' + $(this).parents('.tab_item').attr('data-ds-appid') + '/graphs/"><span class="platform_img steamdb-icon"></span></a>');
+});
 
-	$('.tab_item_cap').each(function(){
-		$(this).children('img').wrap('<a class="screen" target="_blank" href="' + $(this).siblings('.tab_item_overlay').attr('href') + '"></a>');
-	});
+$('.tab_item_cap').each(function() {
+	$(this).children('img').wrap('<a class="screen" target="_blank" href="' + $(this).siblings('.tab_item_overlay').attr('href') + '"></a>');
+});
 
-	$('.screen').ready(function(){
-		$('.tab_item_overlay').hide();
-		$('.tab_item_overlay_hover').hide();		
-	});
+$('.screen').ready(function() {
+	$('.tab_item_overlay').hide();
+	$('.tab_item_overlay_hover').hide();
+});
+
+$('.responsive_page').each(function(){
+	$(this).children('div:first').before('<div class="left_menu"><a target="_blank" href="http://steamcommunity.com/market/"><div class="gomarket">market</div></a><a href="#global_header"><div class="goup">up</div></a></div>');
+});
+
 
 
 //});
